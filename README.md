26175fe2ee194259bc500873e4d194f2d141c55d
harshini <ssharshu2@gmail.com>
2019-07-26 15:39:51 +0000

    added a new print stmt

---
+++ README.md
@@ -0,0 +1,5 @@
+this java program is used for voice assistance
+
+language used Java
+
+team mates: harshini
--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+// this program will print a welcome message
+
+	public class Main {
+    	public static void main(String[] args) {
+
+        //prints hello world
+
+	System.out.println("Hello World");
     }
 }
